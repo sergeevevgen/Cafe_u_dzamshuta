517ed011d745b6e59eed0e523da8a52cfcfebd01
Евгений Сергеев <alexevgenii@mail.ru>
2023-03-05 13:50:08 +0400

    Add some files

---
+++ src/main/java/com/cafe/cafe/model/dto/MessageDto.java
@@ -0,0 +1,4 @@
+package com.cafe.cafe.model.dto;
+
+public class MessageDto {
+}
--- src/main/java/com/cafe/cafe/service/CategoryService.java
+++ src/main/java/com/cafe/cafe/service/CategoryService.java
@@ -1,11 +1,13 @@
 package com.cafe.cafe.service;
 
 import com.cafe.cafe.model.Category;
+import com.cafe.cafe.model.dto.CategoryDto;
 import com.cafe.cafe.repository.CategoryRepository;
 import com.cafe.cafe.util.validation.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.transaction.Transactional;
 
@@ -24,6 +26,18 @@ public class CategoryService {
 
     @Transactional
     public Category addCategory(String name, String description) {
+        if (!StringUtils.hasText(name) || !StringUtils.hasText(description)) {
+            throw new IllegalArgumentException("Enter data");
+        }
+        final Category category = new Category();
+        category.setName(name);
+        category.setDescription(description);
+        validatorUtil.validate(category);
+        return repository.save(category);
+    }
 
+    @Transactional
+    public CategoryDto addCategory(CategoryDto categoryDto) {
+        return new CategoryDto(addCategory(categoryDto.get));
     }
 }
