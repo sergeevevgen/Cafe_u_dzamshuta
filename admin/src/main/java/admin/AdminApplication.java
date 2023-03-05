9fa9e0806eb7fe69e250a900eb31a0270221b682
Евгений Сергеев <alexevgenii@mail.ru>
2023-03-05 15:45:12 +0400

    Add some files

--- admin/src/main/java/admin/AdminApplication.java
+++ admin/src/main/java/admin/AdminApplication.java
@@ -1,5 +1,12 @@
 package admin;
 
-public class AdminApplication {
+import data.model.Category;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
 }
