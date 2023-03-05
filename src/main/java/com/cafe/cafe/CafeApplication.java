e20b383f7523f853580738d345b87b11489b3801
AntonovDevelop <antonov04092002@mail.ru>
2023-03-05 12:54:40 +0400

    начало положено пока без spring security

--- src/main/java/com/cafe/cafe/CafeApplication.java
+++ src/main/java/com/cafe/cafe/CafeApplication.java
@@ -2,7 +2,9 @@ package com.cafe.cafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"admin", "client", "manager", "com.cafe.cafe"})
 @SpringBootApplication
 public class CafeApplication {
 	public static void main(String[] args) {
---
+++ src/main/java/com/cafe/cafe/service/LoginService.java
@@ -0,0 +1,8 @@
+package com.cafe.cafe.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+}
